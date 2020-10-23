8b18f022268b34d1a7c89d121d03d25022f778ee
etech <raman@excellencetetchnologies.in>
2020-10-23 14:41:40 +0530

    Stopwatch Assignment

---
+++ src/components/Control.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="button in buttons" :key="button.id">
+        <button @click="$emit(button.name)">{{ button.name }}</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "control",
+  data() {
+    return {
+      buttons: [
+        { id: 1, name: "Start" },
+        { id: 2, name: "Pause" },
+        { id: 3, name: "Reset" },
+      ],
+    };
+  },
+};
+</script>
+<style>
+li {
+  display: inline;
+}
+</style>
---
+++ src/components/Timer.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p>{{stopwatch}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Timer",
+  props: {
+    stopwatch:{
+      default: "00:00:00",
+    }
+  },
+  // data() {
+  //   return {
+  //     elapsedTime: 0,
+  //     timer: undefined,
+  //   };
+  // },
+  // computed: {
+  //   formattedElapsedTime() {
+  //     const date = new Date(null);
+  //   //   console.log(date);
+  //   //   console.log(this.elapsedTime);
+  //   //   console.log(date.setSeconds(this.elapsedTime / 1000));
+  //     date.setSeconds(this.elapsedTime / 1000);
+  //     const utc = date.toUTCString();
+  //   //   console.log(utc);
+  //   //   console.log(utc.substr(utc.indexOf(":") - 2, 8));
+  //     return utc.substr(utc.indexOf(":") - 2, 8);
+  //   },
+  // },
+};
+</script>
